203cf3f431ad2f4dc35ced7a3b1436e40914087d
noayair <noashefer5@gmail.com>
2020-12-15 23:38:44 +0200

    initush

---
+++ Assets/Scripts/2-player/KeyboardMover.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+/**
+ * This component allows the player to move by clicking the arrow keys.
+ */
+public class KeyboardMover : MonoBehaviour {
+
+    protected Vector3 NewPosition() {
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            return transform.position + Vector3.left;
+        } else if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            return transform.position + Vector3.right;
+        } else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            return transform.position + Vector3.down;
+        } else if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            return transform.position + Vector3.up;
+        } else {
+            return transform.position;
+        }
+    }
+
+    protected Vector3 NewPositionX()
+    {
+        if (Input.GetKey(KeyCode.X))
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                return transform.position + Vector3.left;
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                return transform.position + Vector3.right;
+            }
+            else if (Input.GetKey(KeyCode.DownArrow))
+            {
+                return transform.position + Vector3.down;
+            }
+            else if (Input.GetKey(KeyCode.UpArrow))
+            {
+                return transform.position + Vector3.up;
+            }
+            else
+            {
+                return transform.position;
+            }
+        }
+        else
+        {
+            return transform.position;
+        }
+    }
+
+
+    void Update()  {
+        transform.position = NewPosition();
+    }
+}
---
+++ Assets/Scripts/2-player/KeyboardMoverByTile.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+using System.Collections;
+
+/**
+ * This component allows the player to move by clicking the arrow keys,
+ * but only if the new position is on an allowed tile.
+ */
+public class KeyboardMoverByTile: KeyboardMover {
+    [SerializeField] Tilemap tilemap = null;
+    [SerializeField] AllowedTiles allowedTiles = null;
+    [SerializeField] TileBase tile = null;
+
+    private TileBase TileOnPosition(Vector3 worldPosition) {
+        Vector3Int cellPosition = tilemap.WorldToCell(worldPosition);
+        return tilemap.GetTile(cellPosition);
+    }
+
+    IEnumerator Func(Vector3 pos)
+    {
+        //Waiting one second to carve in the mountain
+        yield return new WaitForSecondsRealtime(1);
+        //Replace the tile from mountain to grass
+        Vector3Int cellPosition = tilemap.WorldToCell(pos);
+        tilemap.SetTile(cellPosition, tile);
+    }
+
+    void Update()  {
+        Vector3 newPositionX = NewPositionX();
+        TileBase tileOnNewXPosition = TileOnPosition(newPositionX);
+        if(tileOnNewXPosition.name == "mountains")
+        {
+            StartCoroutine(Func(newPositionX));
+        }
+        Vector3 newPosition = NewPosition();
+        TileBase tileOnNewPosition = TileOnPosition(newPosition);
+        if (allowedTiles.Contain(tileOnNewPosition)) {
+            transform.position = newPosition;
+        } else {
+            Debug.Log("You cannot walk on " + tileOnNewPosition + "!");
+        }
+    }
+}
